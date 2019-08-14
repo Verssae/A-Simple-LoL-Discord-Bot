faf0819877c7c3b8d8883797466a19330c90bcb3
verssae <wngksto2714@gmail.com>
2019-08-14 13:41:00 +0900

    add win rate, colours, update.py

--- bot.py
+++ bot.py
@@ -1,24 +1,26 @@
 import discord
 import json
+from updated import check_version
 
 from discord.ext import commands
 
 # get data
 with open("config.json", "r") as f:
-    data = json.load(f)
-    version = data.get("version").strip()
-    apikey = data.get("apikey")
-    token = data.get("token")
-    prefix = data.get("prefix")
+    config = json.load(f)
+    version = config.get("version").strip()
+    apikey = config.get("apikey")
+    token = config.get("token")
+    prefix = config.get("prefix")
 
 bot = commands.Bot(command_prefix=prefix)
 cogs = ['cogs.lol']
 
 if __name__ == '__main__':
+    check_version(config)
     for cog in cogs:
         try:
             bot.load_extension(cog)
         except Exception as error:
             print(f'cog {cog} cannot be loaded. [{error}]')
-
+    
     bot.run(token)
--- cogs/lol.py
+++ cogs/lol.py
@@ -113,20 +113,33 @@ class LoL(commands.Cog):
         self.info = None
         self.apikey = apikey
         self.version = version
+        self.tracked_players = []
         with open('emoji-compact.json', 'rb') as f:
             self.emojies = json.load(f)
+        self.colours = {
+            "IRON" : discord.Colour.from_rgb(217,215,214),
+            "BRONZE" : discord.Colour.from_rgb(192,170,152),
+            "SILVER" : discord.Colour.from_rgb(160,181,182),
+            "GOLD" : discord.Colour.from_rgb(212,194,118),
+            "PLATINUM" : discord.Colour.from_rgb(167,218,196),
+            "DIAMOND" : discord.Colour.from_rgb(115,162,193),
+            "MASTER" : discord.Colour.from_rgb(232,164,253),
+            "GRANDMASTER" : discord.Colour.from_rgb(248,43,62),
+            "CHALLENGER" : discord.Colour.from_rgb(225,225,227)
+        }
                 
-    @commands.command(brief="솔로 큐")
+    @commands.command(brief="개인/2인 랭크 게임")
     async def solo(self, ctx, *, name):
         lol_info = LoLInfo(self.apikey, self.version)
         if lol_info.search(name):
             lol_info.setRankInfo()
             if lol_info.soloq:
-                em = discord.Embed(title="RANKED_SOLO_5x5", colour=discord.Colour.green())
+                em = discord.Embed(title="개인/2인 랭크 게임", colour=self.colours.get(lol_info.getSoloq('tier')))
                 em.set_author(name=f"{lol_info.getUser('name')}", icon_url=LoLInfo.getIconUrl(lol_info.getUser('profileIconId')))
                 em.set_image(url=LoLInfo.getEmblemUrl(lol_info.getSoloq('tier'), lol_info.getSoloq('rank')))
                 em.add_field(name="소환사 레벨", value=f"{lol_info.getUser('summonerLevel')}", inline=True)
                 em.add_field(name="승리", value=f"{lol_info.getSoloq('wins')}", inline=True).add_field(name="패배", value=f"{lol_info.getSoloq('losses')}", inline=True)
+                em.add_field(name="게임 수", value=f"{lol_info.getSoloq('wins')+lol_info.getSoloq('losses')}", inline=True).add_field(name="승률", value=f"{lol_info.getSoloq('wins') / (lol_info.getSoloq('wins')+lol_info.getSoloq('losses')) * 100:.2f}%", inline=True)
                 em.set_footer(text=f"{lol_info.getSoloq('tier')} {lol_info.getSoloq('rank')} {lol_info.getSoloq('leaguePoints')} lp")
                 if lol_info.isActive():
                     em.add_field(name="현재 플레이 중!", value=f"{lol_info.getQueueType()}", inline=True)
@@ -135,7 +148,7 @@ class LoL(commands.Cog):
                 message = await ctx.send(embed=em)
             else:
                 await ctx.invoke(self.bot.get_command('normal'), name=name)
-            if lol_info.getUser('name') == 'Vtz':
+            if self.tracked_players != [] and lol_info.getUser('name') in map(lambda e: e['name'], self.tracked_players):
                 await self.cheerup(message)
 
     @commands.command(brief="일반 게임 유저")
@@ -144,7 +157,7 @@ class LoL(commands.Cog):
         if lol_info.search(name):
             lol_info.setRankInfo()
             
-            em = discord.Embed(title="소환사 정보", colour=discord.Colour.green())
+            em = discord.Embed(title="unranked")
             em.set_author(name=f"{lol_info.getUser('name')}", icon_url=LoLInfo.getIconUrl(lol_info.getUser('profileIconId')))
             em.set_image(url=LoLInfo.getEmblemUrl(None, None))
             em.add_field(name="소환사 레벨", value=f"{lol_info.getUser('summonerLevel')}", inline=True)
@@ -153,6 +166,8 @@ class LoL(commands.Cog):
                 em.add_field(name="플레이중인 챔피언", value=f"{lol_info.getChampionName()[0]}", inline=True).add_field(name="플레이 시간", value=f"{lol_info.getGameTime()}")
                 em.set_thumbnail(url=LoLInfo.getChampionUrl(lol_info.getChampionName()[1]))
             message = await ctx.send(embed=em)
+            if self.tracked_players != [] and lol_info.getUser('name') in map(lambda e: e['name'], self.tracked_players):
+                await self.cheerup(message)
 
     
     # emoji surport
@@ -167,17 +182,18 @@ class LoL(commands.Cog):
             except:
                 pass
         
-    @commands.command(brief="자유랭")
+    @commands.command(brief="자유 랭크 게임")
     async def free(self, ctx, *, name):
         lol_info = LoLInfo(self.apikey, self.version)
         if lol_info.search(name):
             lol_info.setRankInfo()
             if lol_info.flex:
-                em = discord.Embed(title="RANKED_FLEX_SR" , colour=discord.Colour.blue())
+                em = discord.Embed(title="자유 랭크 게임" , colour=self.colours[f"{lol_info.getFlex('tier')}"])
                 em.set_author(name=f"{lol_info.getUser('name')}", icon_url=LoLInfo.getIconUrl(lol_info.getUser('profileIconId')))
                 em.set_image(url=LoLInfo.getEmblemUrl(lol_info.getFlex('tier'), lol_info.getFlex('rank')))
                 em.add_field(name="소환사 레벨", value=f"{lol_info.getUser('summonerLevel')}", inline=True)
                 em.add_field(name="승리", value=f"{lol_info.getFlex('wins')}", inline=True).add_field(name="패배", value=f"{lol_info.getFlex('losses')}", inline=True)
+                em.add_field(name="게임 수", value=f"{lol_info.getFlex('wins')+lol_info.getFlex('losses')}", inline=True).add_field(name="승률", value=f"{lol_info.getFlex('wins') / (lol_info.getFlex('wins')+lol_info.getFlex('losses')) * 100:.2f}%", inline=True)
                 em.set_footer(text=f"{lol_info.getFlex('tier')} {lol_info.getFlex('rank')} {lol_info.getFlex('leaguePoints')} lp")
                 if lol_info.isActive():
                     em.add_field(name="현재 플레이 중!", value=f"{lol_info.getQueueType()}", inline=True)
@@ -186,6 +202,8 @@ class LoL(commands.Cog):
                 message = await ctx.send(embed=em)
             else:
                 await ctx.invoke(self.bot.get_command('normal'), name=name)
+                if self.tracked_players != [] and lol_info.getUser('name') in map(lambda e: e['name'], self.tracked_players):
+                    await self.cheerup(message)
 
     @commands.command(brief="전략적 팀 전투")
     async def chess(self, ctx, *, name):
@@ -193,19 +211,22 @@ class LoL(commands.Cog):
         if lol_info.search(name):
             lol_info.setRankInfo()
             if lol_info.tft:
-                em = discord.Embed(title="RANKED_TFT", colour=discord.Colour.gold())
+                em = discord.Embed(title="롤토체스", colour=self.colours[f"{lol_info.getTft('tier')}"])
                 em.set_author(name=f"{lol_info.getUser('name')}", icon_url=LoLInfo.getIconUrl(lol_info.getUser('profileIconId')))
                 em.set_image(url=LoLInfo.getEmblemUrl(lol_info.getTft('tier'), lol_info.getTft('rank')))
                 em.add_field(name="소환사 레벨", value=f"{lol_info.getUser('summonerLevel')}", inline=True)
                 em.add_field(name="승리", value=f"{lol_info.getTft('wins')}", inline=True).add_field(name="패배", value=f"{lol_info.getTft('losses')}", inline=True)
+                em.add_field(name="게임 수", value=f"{lol_info.getTft('wins')+lol_info.getTft('losses')}", inline=True).add_field(name="승률", value=f"{lol_info.getTft('wins') / (lol_info.getTft('wins')+lol_info.getTft('losses')) * 100:.2f}%", inline=True)
                 em.set_footer(text=f"{lol_info.getTft('tier')} {lol_info.getTft('rank')} {lol_info.getTft('leaguePoints')} lp")
                 if lol_info.isActive():
                     em.add_field(name="현재 플레이 중!", value=f"{lol_info.getQueueType()}", inline=True)
                 message = await ctx.send(embed=em)
             else:
                 await ctx.invoke(self.bot.get_command('normal'), name=name)
+                if self.tracked_players != [] and lol_info.getUser('name') in map(lambda e: e['name'], self.tracked_players):
+                    await self.cheerup(message)
 
-    @commands.command(name='logout', brief='Logs the bot out.')
+    @commands.command(name='logout', brief='봇 연결 해제.')
     @commands.is_owner()
     async def logout(self, ctx):
         await self.bot.logout()
@@ -213,82 +234,82 @@ class LoL(commands.Cog):
     @commands.command(name='login', hidden=True)
     @commands.is_owner()
     async def login(self, ctx):
-        await self.bot.change_presence(activity=discord.Activity(name="훈민이 응원", type=0))
+        await self.bot.change_presence(activity=discord.Activity(name="LoL Info Support", type=0))
 
     @tasks.loop(seconds=60.0, count=None)
     async def track_loop(self):
-        name = self.info.get('name')
-        wins = self.info.get('wins')
-        losses = self.info.get('losses')
-        tier = self.info.get('tier')
-        rank = self.info.get('rank')
-        lp = self.info.get('lp')
-        channel_id = self.info.get('channel')
-        active = self.info.get('active')
-        print(f'tracking {name}')
-        lol_info = LoLInfo(self.apikey, self.version)
-        if lol_info.search(name):
-            lol_info.setRankInfo()
-            new_wins =  lol_info.getSoloq('wins')
-            new_losses = lol_info.getSoloq('losses')
-            new_lp = lol_info.getSoloq('leaguePoints')
-            new_tier = lol_info.getSoloq('tier')
-            new_rank = lol_info.getSoloq('rank')
-            new_active = lol_info.isActive()
-            if new_wins > wins:
-                pp('win')
-                if new_lp > lp:
+        for player in self.tracked_players:
+            channel_id = player['channel']
+            lp = player['lp']
+            name = player['name']
+            print(f'tracking {name}')
+
+            lol_info = LoLInfo(self.apikey, self.version)
+            if lol_info.search(name):
+                lol_info.setRankInfo()
+                new_lp = lol_info.getSoloq('leaguePoints')
+    
+                if lol_info.getSoloq('wins') > player['wins']:
+                    player['wins'] = lol_info.getSoloq('wins')
+                    if new_lp > lp:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 승리하여 +{new_lp - lp}포인트 획득!`"
+                        )
+                        player['lp'] = new_lp
+                    elif new_lp == lp:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 승급전에서 승리하여 승급에 가까워지고 있습니다!`"
+                        )
+                    else:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 {lol_info.getSoloq('tier')} {lol_info.getSoloq('rank')} 로 승급했습니다!!`"
+                        )
+                        player['lp'] = new_lp
+                        player['tier'] = lol_info.getSoloq('tier')
+                        player['rank'] = lol_info.getSoloq('rank')
+                    
+                    
+                elif lol_info.getSoloq('losses') > player['losses']:
+                    pp('lose')
+                    if new_lp > lp:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 안타깝게도 {lol_info.getSoloq('tier')} {lol_info.getSoloq('rank')} 로 강등당했군요?`"
+                        )
+                        player['lp'] = new_lp
+                        player['tier'] = lol_info.getSoloq('tier')
+                        player['rank'] = lol_info.getSoloq('rank')
+
+                    elif new_lp == lp:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 다시하기를 했군요`"
+                        )
+                    else:
+                        message = await self.bot.get_channel(channel_id).send(
+                            f"`{name}님이 아쉬운 패배로 {lp - new_lp}점을 잃었습니다...`"
+                        )
+                        player['lp'] = new_lp
+
+                elif lol_info.isActive() and not player['active']:
                     message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 승리하여 +{new_lp - lp}포인트 획득!`"
+                        f"`{name}님이 게임을 시작했습니다!! 화이팅~!`"
                     )
-                elif new_lp == lp:
+                    player['active'] = 'active'
+                else: 
                     message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 승급전에서 승리하여 승급에 가까워지고 있습니다!`"
+                        f"`{name}님의 정보를 가져오는데 실패했습니다.`"
                     )
-                else:
-                    message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 {new_tier} {new_rank} 로 승급했습니다!!`"
-                    )
-                
-            elif new_losses > losses:
-                pp('lose')
-                if new_lp > lp:
-                    message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 안타깝게도 {new_tier} {new_rank} 로 강등당했군요?`"
-                    )
-                elif new_lp == lp:
-                    message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 다시하기를 했군요`"
-                    )
-                else:
-                    message = await self.bot.get_channel(channel_id).send(
-                        f"`{name}님이 아쉬운 패배로 {lp - new_lp}점을 잃었습니다...`"
-                    )
-            elif new_active and not active:
-                message = await self.bot.get_channel(channel_id).send(
-                    f"`{name}님이 게임을 시작했습니다!! 화이팅~!`"
-                )
-            else: 
-                return
-            
-            self.info['wins']= new_wins
-            self.info['losses'] = new_losses
-            self.info['tier'] = new_tier
-            self.info['rank']=new_rank
-            self.info['lp'] = new_lp
-            self.info['active'] = new_active
-            ctx = await self.bot.get_context(message)
-            cmd = self.bot.get_command("solo")
-            await ctx.invoke(cmd, name=name)
+                ctx = await self.bot.get_context(message)
+                cmd = self.bot.get_command("solo")
+                await ctx.invoke(cmd, name=name)
 
 
-    @commands.command()
-    async def save(self, ctx, *, name, brief="주기적으로 정보를 받아올 사람 저장(1명만 됨)"):
+    @commands.command(brief="1분마다 정보 갱신할 플레이어 저장")
+    async def save(self, ctx, *, name):
         lol_info = LoLInfo(self.apikey, self.version)
         if lol_info.search(name):
             lol_info.setRankInfo()
             if lol_info.soloq:
-                self.info = {
+                info = {
                     'name' : lol_info.getUser('name'),
                     'wins' : lol_info.getSoloq('wins'),
                     'losses' : lol_info.getSoloq('losses'),
@@ -298,8 +319,8 @@ class LoL(commands.Cog):
                     'channel' : ctx.channel.id,
                     'active' : lol_info.isActive()
                 }
+                self.tracked_players.append(info)
                 self.track_loop.cancel()
-                pp(self.info)
                 cmd = self.bot.get_command("solo")
                 await ctx.invoke(cmd, name=name)
                 self.track_loop.start()
